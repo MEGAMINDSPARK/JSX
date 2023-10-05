c4c5b9450bd9ffbc3cc4ab867543706190f7196a
MEGAMINDSPARK <rahulkumarroy399@gmail.com>
2023-10-05 13:06:00 +0530

    Validating the forms (UseEffect)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React, { useState, Fragment } from 'react';
+import React, { useEffect, useState } from "react";
 
-import AddUser from './components/Users/AddUser';
-import UsersList from './components/Users/UsersList';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
-  const [usersList, setUsersList] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const addUserHandler = (uName, uAge) => {
-    setUsersList((prevUsersList) => {
-      return [
-        ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
-      ];
-    });
+  useEffect(() => {
+    const storeUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storeUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+    //this fun will run only oonce when the apps start because there after the dependencies never change becaus here is no dependencies
+  }, []);
+
+  const loginHandler = (email, password,collegeName) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1"); //1 for logged in and 0 for not logged in
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn')
+    setIsLoggedIn(false);
   };
 
   return (
-    <Fragment>
-      <AddUser onAddUser={addUserHandler} />
-      <UsersList users={usersList} />
-    </Fragment>
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
   );
 }
 
